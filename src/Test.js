c649802ffcad1ae683aace06d4742687885c936b
Nick Damico <nick@nickdami.co>
2018-09-30 10:50:44 -0300

    contador bien y mal funcionando

--- src/Test.js
+++ src/Test.js
@@ -9,28 +9,30 @@ class Test extends Component {
         size:3,
         respuestaBien: 0,
         respuestaMal:0,
+        timer: 0,
       };
     }
-      handleClick(){
+      handleClick(ver){
         this.setState({examen: ++this.state.examen});
         this.setterTamanio();
         if (this.state.examen>10) {
           this.resultado();
+        }
+        if (ver) {
+          this.setState({respuestaBien:++this.state.respuestaBien})
+        }else {
+          this.setState({respuestaMal:++this.state.respuestaMal})
         }
       }
       setterTamanio(){
         if (this.state.examen>5) {
           this.setState({size: 4});
         }
-
       }
-    }=
     render() {
       //TODO: ejecutar 5 ejercicios consecutivamente. Guardar el estado de cada ejercicio
       return (
-          <Ejercicio tamanio={3} alTerminar={(acierto) => if(acierto){this.setState({respuestaBien:++this.state.respuestaBien})}else {
-            this.setState({respuestaMal:++this.state.respuestaMal})
-          }}/>
+          <Ejercicio tamanio={3} alTerminar={(acierto) => this.handleClick(acierto)}/>
       );
     }
 }
