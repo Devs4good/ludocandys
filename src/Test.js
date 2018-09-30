668e8998248cef2098e27129032a61598fef9171
Nick Damico <nick@nickdami.co>
2018-09-30 10:36:27 -0300

    contador de iteracion y respuestas

--- src/Test.js
+++ src/Test.js
@@ -2,10 +2,35 @@ import React, { Component } from 'react';
 import Ejercicio from "./Ejercicio";
 
 class Test extends Component {
+    constructor(props){
+      super(props);
+      this.state={
+        examen:1,
+        size:3,
+        respuestaBien: 0,
+        respuestaMal:0,
+      };
+    }
+      handleClick(){
+        this.setState({examen: ++this.state.examen});
+        this.setterTamanio();
+        if (this.state.examen>10) {
+          this.resultado();
+        }
+      }
+      setterTamanio(){
+        if (this.state.examen>5) {
+          this.setState({size: 4});
+        }
+
+      }
+    }=
     render() {
       //TODO: ejecutar 5 ejercicios consecutivamente. Guardar el estado de cada ejercicio
       return (
-          <Ejercicio tamanio={3} alTerminar={(acierto) => console.log(acierto)}/>
+          <Ejercicio tamanio={3} alTerminar={(acierto) => if(acierto){this.setState({respuestaBien:++this.state.respuestaBien})}else {
+            this.setState({respuestaMal:++this.state.respuestaMal})
+          }}/>
       );
     }
 }
