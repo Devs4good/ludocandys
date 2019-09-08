3f0bc12615d7408c147dca7c1ef79374338fceed
kzhama <k@zhama.io>
2019-09-08 12:14:08 -0300

    fix varios

--- src/screens/Games/Game-1/index.js
+++ src/screens/Games/Game-1/index.js
@@ -1,18 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 /* Components */
-import Exercise from '../../../components/Exercise';
-import Speak from '../../../components/SpeakComponent/Speak';
+// import Exercise from '../../../components/Exercise';
+import Speak from "../../../components/SpeakComponent/Speak";
 /* Assets */
-import micIcon from './microphone-solid.svg';
-import stopIcon from './stop-solid.svg';
-import './styles.css';
+import micIcon from "./microphone-solid.svg";
+// import stopIcon from './stop-solid.svg';
+import "./styles.css";
 /* Utils */
-import removeAccents from '../../../utils/accents';
+import removeAccents from "../../../utils/accents";
 
 const phrasesPreloaded = {
-  1: { id: '1', text: 'Un barco chico navega por el mar', didWin: null },
-  2: { id: '2', text: 'La paloma se poso en el palo', didWin: null },
+  1: { id: "1", text: "Un barco chico navega por el mar", didWin: null },
+  2: { id: "2", text: "La paloma se poso en el palo", didWin: null }
 };
 
 const QueLees = ({ history }) => {
@@ -22,24 +22,27 @@ const QueLees = ({ history }) => {
   const [canTalk, setCanTalk] = useState(false);
 
   useEffect(() => {
-    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-    const SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList;
-    const testBtn = document.querySelector('.start-button');
+    const SpeechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+    const SpeechGrammarList =
+      window.SpeechGrammarList || window.webkitSpeechGrammarList;
+    const testBtn = document.querySelector(".start-button");
     let didWinCurrentPhrase = null;
 
     function testSpeech() {
       testBtn.disabled = true;
-      testBtn.textContent = 'Prueba en progreso';
+      testBtn.textContent = "Prueba en progreso";
 
       let phrase = chosenPhrase.text.toLowerCase();
 
-      const grammar = '#JSGF V1.0; grammar phrase; public <phrase> = ' + chosenPhrase + ';';
+      const grammar =
+        "#JSGF V1.0; grammar phrase; public <phrase> = " + chosenPhrase + ";";
       const recognition = new SpeechRecognition();
       const speechRecognitionList = new SpeechGrammarList();
 
       speechRecognitionList.addFromString(grammar, 1);
       recognition.grammars = speechRecognitionList;
-      recognition.lang = 'es-ES';
+      recognition.lang = "es-ES";
       recognition.interimResults = false;
       recognition.maxAlternatives = 1;
 
@@ -51,37 +54,43 @@ const QueLees = ({ history }) => {
 
       recognition.onresult = function(event) {
         const speechResult = event.results[0][0].transcript.toLowerCase();
-        console.log('TCL: recognition.onresult -> speechResult', speechResult);
+        console.log("TCL: recognition.onresult -> speechResult", speechResult);
 
         if (removeAccents(speechResult) === phrase.toLowerCase()) {
           setPhrases(prevPhrases => ({
             ...prevPhrases,
-            [chosenPhrase.id]: { ...prevPhrases[chosenPhrase.id], didWin: true },
+            [chosenPhrase.id]: { ...prevPhrases[chosenPhrase.id], didWin: true }
           }));
 
           setPoints(prevPoints => prevPoints + 5);
-          Speak('Felicitaciones sumaste 5 puntos');
+          Speak("Felicitaciones sumaste 5 puntos");
           didWinCurrentPhrase = true;
         } else {
           setPhrases(prevPhrases => ({
             ...prevPhrases,
-            [chosenPhrase.id]: { ...prevPhrases[chosenPhrase.id], didWin: false },
+            [chosenPhrase.id]: {
+              ...prevPhrases[chosenPhrase.id],
+              didWin: false
+            }
           }));
-          Speak('Prueba de nuevo!');
+          Speak("Prueba de nuevo!");
           didWinCurrentPhrase = false;
         }
       };
 
       recognition.onspeechend = function() {
         testBtn.disabled = false;
-        testBtn.textContent = 'Empezar nuevo test';
+        testBtn.textContent = "Empezar nuevo test";
         setCanTalk(false);
-        console.log('TCL: recognition.onspeechend -> didWinCurrentPhrase', didWinCurrentPhrase);
+        console.log(
+          "TCL: recognition.onspeechend -> didWinCurrentPhrase",
+          didWinCurrentPhrase
+        );
 
         setTimeout(() => {
           if (didWinCurrentPhrase) {
             const nextPhrase = phrases[Number(chosenPhrase.id) + 1];
-            if (!nextPhrase) history.push('dictado');
+            if (!nextPhrase) history.push("dictado");
             setChosenPhrase(nextPhrase);
           }
 
@@ -90,30 +99,30 @@ const QueLees = ({ history }) => {
       };
     }
 
-    testBtn.addEventListener('click', testSpeech);
+    testBtn.addEventListener("click", testSpeech);
   });
 
   return (
-    <div id='dictation-page' className='speech-page'>
-      <div className='dictation-exercise speech-exercise'>
+    <div id="dictation-page" className="speech-page">
+      <div className="dictation-exercise speech-exercise">
         <header>
           <h2>Decí lo que lees</h2>
           <h3>Ejercicio {chosenPhrase.id}</h3>
         </header>
 
-        <div className='speech-reco-text'>{chosenPhrase.text}</div>
+        <div className="speech-reco-text">{chosenPhrase.text}</div>
 
         <div>
-          <button onClick={() => {}} className='start-button'>
-            <img src={micIcon} />
+          <button onClick={() => {}} className="start-button">
+            <img src={micIcon} alt="" />
           </button>
           {canTalk && <p>Puedes empezar a hablar</p>}
         </div>
       </div>
 
-      <div className='dictation-score speech-score'>
-        <div className='dictation-score-title'>PUNTOS</div>
-        <div className='dictation-score-number'>{points}</div>
+      <div className="dictation-score speech-score">
+        <div className="dictation-score-title">PUNTOS</div>
+        <div className="dictation-score-number">{points}</div>
       </div>
     </div>
   );
--- src/screens/Games/Game-3/index.js
+++ src/screens/Games/Game-3/index.js
@@ -1,60 +1,60 @@
-import React, { useState, useMemo, useEffect } from 'react';
-import { Redirect } from 'react-router-dom';
+import React, { useState, useMemo, useEffect } from "react";
+// import { Redirect } from 'react-router-dom';
 
 /* Styles */
-import './styles.css';
+import "./styles.css";
 
 const exercises = {
   1: {
     id: 1,
-    img: 'https://static.nationalgeographic.es/files/styles/image_470/public/2928.600x450.jpg',
+    img:
+      "https://static.nationalgeographic.es/files/styles/image_470/public/2928.600x450.jpg",
     options: [
-      { text: 'elefante', isValid: true },
-      { text: 'elafente', isValid: false },
-      { text: 'elefenta', isValid: false },
-      { text: 'alefente', isValid: false },
-      { text: 'elefnte', isValid: false },
-    ],
+      { text: "elefante", isValid: true },
+      { text: "elafente", isValid: false },
+      { text: "elefenta", isValid: false },
+      { text: "alefente", isValid: false },
+      { text: "elefnte", isValid: false }
+    ]
   },
   2: {
     id: 2,
-    img: 'https://www.savethekoala.com/sites/savethekoala.com/files/uploads/koala_donate.jpg',
+    img:
+      "https://www.savethekoala.com/sites/savethekoala.com/files/uploads/koala_donate.jpg",
     options: [
-      { text: 'koala', isValid: true },
-      { text: 'kaola', isValid: false },
-      { text: 'klaoa', isValid: false },
-      { text: 'kalao', isValid: false },
-      { text: 'kaalo', isValid: false },
-    ],
-  },
+      { text: "koala", isValid: true },
+      { text: "kaola", isValid: false },
+      { text: "klaoa", isValid: false },
+      { text: "kalao", isValid: false },
+      { text: "kaalo", isValid: false }
+    ]
+  }
 };
 
 const Game3 = props => {
-  const [selectedOption, setSelectedOption] = useState('');
-  console.log('TCL: selectedOption', selectedOption);
+  const [selectedOption, setSelectedOption] = useState("");
+  console.log("TCL: selectedOption", selectedOption);
 
   const {
     match: {
-      params: { id },
-    },
+      params: { id }
+    }
   } = props;
 
-  const selectedExcercise = useMemo(
-    () => {
-
-      if (!exercises[id]) return exercises[1];
-      return { ...exercises[id], options: exercises[id].options.sort(() => 0.5 - Math.random()) }
-
-    },
-    [id]
-  );
+  const selectedExcercise = useMemo(() => {
+    if (!exercises[id]) return exercises[1];
+    return {
+      ...exercises[id],
+      options: exercises[id].options.sort(() => 0.5 - Math.random())
+    };
+  }, [id]);
 
   useEffect(() => {
     if (selectedOption.isValid) {
       setTimeout(() => props.history.push("/juegos/que-ves/2"), 1000);
     }
+    //eslint-disable-next-line
   }, [selectedOption]);
-
 
   /*
     1. Si fallo lo ponemos en rojo
@@ -62,30 +62,33 @@ const Game3 = props => {
   */
 
   return (
-    <div className='game-container'>
-      <div className='container'>
+    <div className="game-container">
+      <div className="container">
         <header>
           <h2>Selecciona lo que ves</h2>
           <h3>Ejercicio {id}</h3>
         </header>
 
-        <div className='options-container'>
-          <div className='exercise-image'>
-            <img src={selectedExcercise.img} alt='-' />
+        <div className="options-container">
+          <div className="exercise-image">
+            <img src={selectedExcercise.img} alt="-" />
           </div>
 
-          <div className='exercise-options'>
+          <div className="exercise-options">
             {selectedExcercise.options.map(option => {
               return (
                 <label
                   className={`exercise-option ${
-                    selectedOption.text === option.text && selectedOption.isValid === true ? 'valid' : 'invalid'
-                    }`}
+                    selectedOption.text === option.text &&
+                    selectedOption.isValid === true
+                      ? "valid"
+                      : "invalid"
+                  }`}
                   key={option.text}
                 >
                   {option.text}
                   <input
-                    type='radio'
+                    type="radio"
                     name={option.text}
                     value={option.text}
                     checked={selectedOption.text === option.text}
